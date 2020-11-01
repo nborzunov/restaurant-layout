3959400de3d1fd10664ec010848b3d85c3daa6f0
Nikolay Borzunov <forest3694@ya.ru>
2020-11-01 14:08:17 +0300

    responsive 0.11

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,9 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if ((prevScrollpos > currentScrollPos) && (window.innerWidth>960)) {
     document.getElementById("navbar").style.top = "0";
-  } else {
+  } else if ((prevScrollpos < currentScrollPos) && (window.innerWidth>960)){
     document.getElementById("navbar").style.top = "-350px";
   }
   prevScrollpos = currentScrollPos;
