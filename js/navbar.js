bbdd9f740575d313a7bc6b817017bcae6582321f
Nikolay Borzunov <forest3694@ya.ru>
2020-11-02 18:33:12 +0300

    responsive 0.13

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,6 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
+
   var currentScrollPos = window.pageYOffset;
   var mm = document.getElementById('mobileMenu');
   var mc = document.getElementById('mobileContainer');
@@ -8,33 +9,27 @@ window.onscroll = function() {
   var sidebar = document.getElementById('sidebar');
   var menubar = document.getElementById('menubar');
 
-  
-  if ((prevScrollpos > currentScrollPos) && (window.innerWidth>960)) {
-    document.getElementById("navbar").style.top = "0";
-  } else if ((prevScrollpos < currentScrollPos) && (window.innerWidth>960)){
-    document.getElementById("navbar").style.top = "-350px";
+  if ((window.pageYOffset%20==0) || (window.pageYOffset==0)){
+    if ((prevScrollpos > currentScrollPos) && (window.innerWidth>960) && (window.pageYOffset<=199)) {
+      menubar.style.top = "0";
+    }
+    if ((prevScrollpos < currentScrollPos) && (window.innerWidth>960)){
+      menubar.style.top = "-350px";
+    }
+    if ((prevScrollpos < currentScrollPos) && (window.innerWidth>960) && (window.pageYOffset>=200)){
+      sidebar.style.top = "0";
+    }
+    if ((prevScrollpos > currentScrollPos) && (window.innerWidth>960) && (window.pageYOffset<=200)){
+      sidebar.style.top = "-200px";
+    }
+    prevScrollpos = currentScrollPos;
+    if (mm.classList.contains('active')){
+      mm.classList.toggle('active');
+      mc.classList.toggle('change');
+    }
+    if (dm.classList.contains('active')){
+      dm.classList.toggle('active');
+      dc.classList.toggle('change');
+    }
   }
-  prevScrollpos = currentScrollPos;
-  if (mm.classList.contains('active')){
-    mm.classList.toggle('active');
-    mc.classList.toggle('change');
-  }
-  if (dm.classList.contains('active')){
-    dm.classList.toggle('active');
-    dc.classList.toggle('change');
-  }
-
-  if (window.pageYOffset<=120){
-    sidebar.classList.add('none')
-  }
-  if (window.pageYOffset>120){
-    sidebar.classList.remove('none')
-  }
-  if (window.pageYOffset<=120){
-    menubar.classList.remove('none')
-  }
-  if (window.pageYOffset>120){
-    menubar.classList.add('none')
-  }
-  
 }
