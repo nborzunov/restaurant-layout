9999f2066a281780d39e2f76d994e24da7974903
Nikolay Borzunov <forest3694@ya.ru>
2020-11-02 10:22:41 +0300

    responsive 0.12

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,40 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
+  var mm = document.getElementById('mobileMenu');
+  var mc = document.getElementById('mobileContainer');
+  var dm = document.getElementById('desktopMenu');
+  var dc = document.getElementById('desktopContainer');
+  var sidebar = document.getElementById('sidebar');
+  var menubar = document.getElementById('menubar');
+
+  
   if ((prevScrollpos > currentScrollPos) && (window.innerWidth>960)) {
     document.getElementById("navbar").style.top = "0";
   } else if ((prevScrollpos < currentScrollPos) && (window.innerWidth>960)){
     document.getElementById("navbar").style.top = "-350px";
   }
   prevScrollpos = currentScrollPos;
-  if (document.getElementById('menu').classList.contains('active')){
-    document.getElementById('menu').classList.toggle('active')
-    document.getElementById('container').classList.toggle('change')
+  if (mm.classList.contains('active')){
+    mm.classList.toggle('active');
+    mc.classList.toggle('change');
+  }
+  if (dm.classList.contains('active')){
+    dm.classList.toggle('active');
+    dc.classList.toggle('change');
+  }
+
+  if (window.pageYOffset<=120){
+    sidebar.classList.add('none')
+  }
+  if (window.pageYOffset>120){
+    sidebar.classList.remove('none')
+  }
+  if (window.pageYOffset<=120){
+    menubar.classList.remove('none')
+  }
+  if (window.pageYOffset>120){
+    menubar.classList.add('none')
   }
   
 }
